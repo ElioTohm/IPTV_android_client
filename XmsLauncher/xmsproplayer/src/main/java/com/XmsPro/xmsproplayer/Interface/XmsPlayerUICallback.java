36a953cb200a5854953e4ad4c7bf718824e2bfb5
Elio Tohme <eliotohme@gmail.com>
2017-09-29 11:08:26 +0300

    added player callback to version control

---
+++ XmsLauncher/xmsproplayer/src/main/java/com/XmsPro/xmsproplayer/Interface/XmsPlayerUICallback.java
@@ -0,0 +1,12 @@
+package com.XmsPro.xmsproplayer.Interface;
+
+/**
+ * Created by Elio on 9/28/2017.
+ */
+
+public interface XmsPlayerUICallback {
+
+    void showChannelInfo(int channelindex);
+
+
+}
