cb29520e5be05bb79860bf4358eb86f7552a2c7d
elio <eliotohme@gmail.com>
2018-02-10 12:31:46 +0200

    added date time in  menu fragment

--- XmsLauncher/smarttv/src/main/java/xms/com/smarttv/fragments/SectionMenuFragment.java
+++ XmsLauncher/smarttv/src/main/java/xms/com/smarttv/fragments/SectionMenuFragment.java
@@ -59,7 +59,7 @@ public class SectionMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_channel_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_section_list, container, false);
         view.findViewById(R.id.channel_recycler_view);
         // Set the adapter
         Context context = view.getContext();
