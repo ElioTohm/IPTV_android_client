2c05a5aa2a21713a9d2973294ec6311442171493
elio <eliotohme@gmail.com>
2018-03-15 13:00:29 +0200

    bug fix

--- XmsLauncher/data/src/main/java/com/eliotohme/data/Purchase.java
+++ XmsLauncher/data/src/main/java/com/eliotohme/data/Purchase.java
@@ -4,6 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
 
 /**
  * Purchase Object
@@ -11,7 +12,7 @@ import io.realm.RealmObject;
  * and retrieved in response to update purchased item on client side
  */
 public class Purchase extends RealmObject {
-
+    @PrimaryKey
     @SerializedName("id")
     @Expose
     private Integer id;
